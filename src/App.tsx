037540d0cad7a01b03776f02d6f4201ddb366781
HanninenF <hanninenfredrik@gmail.com>
2025-02-21 21:25:31 +0100

    update> app

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       {/* <TextInputForCards /> */}
-      <MonsterCard />
+      {/* <MonsterCard /> */}
       {/*  <RandomDeckOfCards /> */}
       <RandomCard />
     </>
